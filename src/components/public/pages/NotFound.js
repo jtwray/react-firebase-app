85894c93d80c6a39ab55c7029aac0d7499be4e55
Chaoming Li <email@chaoming.li>
2019-10-03 11:21:00 +1000

    polished not found page

--- src/components/public/pages/NotFound.js
+++ src/components/public/pages/NotFound.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../public.css';
+import { Link } from "react-router-dom";
 
 const NotFound = ({ children }) => (
     <div className="public-layout text-center">
         <div className="public-main">
-            Oops, the page you requested is not found.
+            <h1>Page not found</h1>
+            <p>Oops, the page you requested is not found.</p>
+            <Link to="/">Home</Link>
         </div>
     </div>
 );
