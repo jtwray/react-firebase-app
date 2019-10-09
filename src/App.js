77d95202770d3b5335252016b4b6740dde5e73ca
Chaoming Li <email@chaoming.li>
2019-10-10 10:28:17 +1100

    added update email

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import NotFound from './components/public/pages/NotFound';
 import UserProfile from './components/app/pages/user/UserProfile';
 import UpdateUserName from './components/app/pages/user/UpdateUserName';
 import UpdateUserPhone from './components/app/pages/user/UpdateUserPhone';
+import UpdateUserEmail from './components/app/pages/user/UpdateUserEmail';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <AppLayoutRouter exact path="/" component={Home} />
           <AppLayoutRouter exact path="/user/profile" component={UserProfile} />
           <AppLayoutRouter exact path="/user/update-name" component={UpdateUserName} />
+          <AppLayoutRouter exact path="/user/update-email" component={UpdateUserEmail} />
           <AppLayoutRouter exact path="/user/update-phone" component={UpdateUserPhone} />
           <PublicLayoutRouter exact path="/signin" component={SignIn} />
           <Route component={NotFound} />
---
+++ src/components/app/pages/user/UpdateUserEmail.js
@@ -0,0 +1,89 @@
+import React, {useContext, useState} from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../../auth/FirebaseAuthContext";
+import FirebaseAuth from '../../../auth/FirebaseAuth';
+import Alert from '../../../Alert';
+
+const UpdateUserEmail = () => {
+    
+  const {authUser} = useContext(AuthContext);
+  const [data, setData] = useState({
+      'emailAddress': authUser.user.email||''
+  });
+  const [formVisibility, setFormVisibility] = useState(true);
+  const [processing, setProcessing] = useState(false);
+  const [alert, setAlert] = useState({
+    'show': false,
+    'style':'',
+    'message':'',
+    'count': 0
+  });
+  
+  var title = 'Change Your Email';
+
+  document.title = title;
+
+  return (
+    <div className="container-fluid">
+      <div className="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 className="h3 mb-0 text-gray-800">{title}</h1>
+      </div>
+      <div className="row">      
+        <Alert show={alert.show} style={alert.style} message={alert.message} count={alert.count} />
+      </div>
+      {formVisibility?(
+      <div className="row">
+        <div className="col mb-4">
+            <div className="card shadow mb-4">
+                <div className="card-body">
+                    <div className="form-group row">
+                        <label htmlFor="email-address" className="col-sm-2 col-form-label">Email</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control" id="email-address" aria-describedby="email-address-help" placeholder="Your full name" value={data.emailAddress} onChange={(e) => {
+                                setData({
+                                    'emailAddress': e.target.value
+                                })
+                            }} />
+                            <small id="email-address-help" className="form-text text-muted">Please put in your new email address.</small>
+                        </div>
+                    </div>
+                    <div className="text-center">
+                        <button type="submit" className="btn btn-primary" disabled={processing?true:false} onClick={(e) => {
+                            e.preventDefault();
+                            setProcessing(true);
+                            FirebaseAuth.auth().currentUser.updateEmail(data.emailAddress).then(function(){
+                                setProcessing(false);
+                                setFormVisibility(false);
+                                setAlert({
+                                    'show':true, 
+                                    'style':'success',
+                                    'message':'Your email address has been updated. Please click "Back" button to go back to your profile page.',
+                                    'count':alert.count+1
+                                });
+                            }).catch(function(error){
+                                setProcessing(false);
+                                setAlert({
+                                    'show':true, 
+                                    'style':'danger',
+                                    'message':error.message,
+                                    'count':alert.count+1
+                                });
+                            })
+                        }}>{processing?(
+                            <i className="fa fa-spinner fa-spin"></i>
+                        ):(
+                            <></>
+                        )} Submit</button>
+                        &nbsp;
+                        <Link className="btn btn-secondary" to="/user/profile">Back</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+      ):(<Link className="btn btn-secondary" to="/user/profile">Back</Link>)}
+    </div>
+  );
+};
+
+export default UpdateUserEmail;
